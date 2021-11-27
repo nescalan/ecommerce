c1c0d78d7d296d79063ecb87abdb3d71c67296a4
Ing. Nelson Gonzalez Escalante <nescalan@gmail.com>
2021-11-27 17:26:17 -0600

    Styled Componets Instaled

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,12 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Jumbo from "../components/Jumbo"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
+    <Jumbo />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
